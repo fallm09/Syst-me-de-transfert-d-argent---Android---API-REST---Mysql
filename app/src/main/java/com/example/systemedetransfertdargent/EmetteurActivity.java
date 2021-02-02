19aadc1620d766e670f006cf5bc414dff941081c
fallm09 <fallmena09@gmail.com>
2021-02-02 10:59:51 +0100

    first commit

---
+++ app/src/main/java/com/example/systemedetransfertdargent/EmetteurActivity.java
@@ -0,0 +1,15 @@
+package com.example.systemedetransfertdargent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class EmetteurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emetteur);
+    }
+}
